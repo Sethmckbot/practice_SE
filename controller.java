4cfcaef44043c57897b1fc59edfbb5594ac88315
Seth <mjz30.txstate.edu>
2024-10-18 15:46:05 -0500

    added prompt and scanner

--- controller.java
+++ controller.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class controller {
 
     public static void main(String[] args) {
-        System.out.println("Im Hungry!");
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Hello, Welcome to Fast Food ordering, How can I help you?");
+            System.out.println("1. Order Meal \n2. Order Drink \n3. Other \n");
+            System.out.println("Your Answer: ");
+
+            String userinput = input.nextLine();
+
+
+
+            System.out.println(userinput);
+        } catch (Exception e) {
+            System.out.println("Error with Scanner");
+        }
     }
 
 }
